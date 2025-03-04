54ad4b17cfa9e1dd9033e6b3ef67dc1a06374114
BartKupc <kupcbart@gmail.com>
2025-03-04 21:03:31 +0000

    new version 9

--- strategies/live_moment2.py
+++ strategies/live_moment2.py
@@ -57,7 +57,7 @@ params = {
     'ema_period': 9,
     'ema_threshold': 0.006,
     'stop_loss_pct': 0.01,      # 1% stop loss
-    'take_profit_pct': 0.04,    # Changed from 10% to 2% for more frequent profits
+    'take_profit_pct': 0.02,    # Changed from 10% to 2% for more frequent profits
     'leverage': 3,
     'max_short_duration': 48,
     'short_underwater_threshold': -0.02,
